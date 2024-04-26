680ca836e49de538afb425de1205f61b4a09d213
Brenden Hartman <brendenhart22@gmail.com>
2024-04-26 13:46:27 -0500

    created an anagram class that takes lists and words and checks for anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        matches = []
+        word_letters = [letter for letter in self.word]
+        for word in list:
+            list_letters = [letter for letter in word]
+            if sorted(list_letters) == sorted(word_letters):
+                matches.append(word)
+        return matches
